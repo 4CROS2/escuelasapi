408c96a91491c5cd299093e0b1606a530f6da7ae
yeferson rivas <yeferok2@gmail.com>
2023-09-17 21:41:07 -0500

    final api

--- lib/pages/home/parts/products.dart
+++ lib/pages/home/parts/products.dart
@@ -1,6 +1,7 @@
 import 'package:escuelasapi/bloc/app/app_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:masonry_grid/masonry_grid.dart';
 
 class Products extends StatefulWidget {
@@ -17,23 +18,64 @@ class _ProductsState extends State<Products> {
       builder: (context, state) {
         if (state is InitializedApi) {
           return MasonryGrid(
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
             column: 2,
             children: [
               ...List.generate(state.dataApi.length, (index) {
-                return Container(
-                  clipBehavior: Clip.hardEdge,
-                  decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(12), boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(.12),
-                      blurRadius: 12,
-                      offset: const Offset(2, 10),
-                    )
-                  ]),
-                  child: Column(
-                    children: [
-                      Image.network(state.dataApi[index].images.first)
-                    ],
-                  ),
+                return FutureBuilder(
+                  future: Future.delayed(Duration(milliseconds: 100 * index)),
+                  builder: (context, snapshot) {
+                    switch (snapshot.connectionState) {
+                      case ConnectionState.waiting:
+                        return const SizedBox.shrink();
+                      default:
+                        return AnimatedOpacity(
+                          duration: const Duration(milliseconds: 200) ,
+                          opacity: snapshot.connectionState == ConnectionState.done ? 1 : 0,
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+                            clipBehavior: Clip.hardEdge,
+                            decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(12), boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(.12),
+                                blurRadius: 12,
+                                offset: const Offset(2, 10),
+                              )
+                            ]),
+                            child: Column(
+                              children: [
+                                /* CachedNetworkImage(
+                                  imageUrl: state.dataApi[index].images.first,
+                                  placeholder: (context, url) => const CircularProgressIndicator(),
+                                  errorWidget: (context, url, error) => const Icon(Icons.error),
+                                ),
+                                 */
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(7),
+                                  child: Image.network(
+                                    state.dataApi[index].images.first,
+                                  ),
+                                ),
+                                Container(
+                                  alignment: Alignment.center,
+                                  height: 50,
+                                  child: Text(
+                                    state.dataApi[index].title,
+                                    textAlign: TextAlign.center,
+                                    style: GoogleFonts.redHatText(
+                                      textStyle: const TextStyle(
+                                        fontWeight: FontWeight.w700,
+                                      ),
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        );
+                    }
+                  },
                 );
               }),
             ],
